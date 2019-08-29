d962d7eec5c11d2acddf83f224647ecd1997b536
Greg <gpr.witczak@gmail.com>
2019-08-29 14:45:33 +0200

    added img to card markup

--- src/js/views/cardListView.js
+++ src/js/views/cardListView.js
@@ -16,6 +16,7 @@ class CardView extends View {
     getCardMarkup(cardObj) {
         return `
             <article class="card">
+                <img class="card__image" src="${cardObj.img ? cardObj.img : "./../../assets/templateCard.png"}" alt="${ cardObj.name ? cardObj.name : cardObj.cardId }">
                 <h4 class="card__title">${ cardObj.name ? cardObj.name : cardObj.cardId }</h4>
                 <p class="card__description">${ cardObj.text ? cardObj.text : 'No text' }</p>
             </article>
