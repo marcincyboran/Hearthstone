5245875f60edc041dbc73f4fd4cca5e566d04028
unknown <aleksandra.przytula1995@gmail.com>
2019-09-01 12:16:53 +0200

    stuffv1

--- src/js/views/cardListView.js
+++ src/js/views/cardListView.js
@@ -21,6 +21,7 @@ class CardView extends View {
                         class="card__image" 
                         src="${ cardObj.img ? cardObj.img : "./../../assets/backupcard.png"}"
                         alt="${ cardObj.name ? cardObj.name : cardObj.cardId }" >
+
                     <img 
                         class="card__image-back" 
                         src="/../../assets/back.png"
@@ -28,6 +29,14 @@ class CardView extends View {
                 </figure>
                 <h4 class="card__title">${ cardObj.name ? cardObj.name : cardObj.cardId }</h4>
                 <p class="card__description">${ cardObj.text ? cardObj.text : 'No text' }</p>
+                <p class="card__rarity">${ cardObj.rarity ? ("Rarity: " + cardObj.rarity) : 'Rarity: No info'}</p>
+                <p class="card__cardSet">${ cardObj.cardSet ? ("Card Set: " + cardObj.cardSet) : 'Card Set: No info'}</p>
+                <p class="card__type">${ cardObj.type ? ("Type: " + cardObj.type) : 'Type: No info'}</p>
+                <p class="card__cost">${ cardObj.cost ? ("Cost: " + cardObj.cost) : 'Cost: No info'}</p>
+                <p class="card__attack">${ cardObj.attack ? ("Attact: " + cardObj.attack) : 'Attack: No info'}</p>
+                <p class="card__health">${ cardObj.health ? ("Health: " + cardObj.health) : 'Health: No info'}</p>
+                <p class="card__race">${ cardObj.race ? ("Race: " + cardObj.race) : 'Race: No info'}</p>
+                <p class="card__flavor">${ cardObj.flavor ? ("Flavor: " + cardObj.flavor) : 'Flavor: No info'}</p>
             </article>
         `
     }
