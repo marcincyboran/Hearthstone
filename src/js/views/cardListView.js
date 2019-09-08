b36e8ab1b623b2fe09324aa5a448f332f3da0f31
unknown <aleksandra.przytula1995@gmail.com>
2019-09-08 20:46:29 +0200

    style

--- src/js/views/cardListView.js
+++ src/js/views/cardListView.js
@@ -13,6 +13,40 @@ class CardView extends View {
         return output;
     }
 
+    getCardMore(cardObj) {
+        return `
+            <article class="card" data-cardId="${ cardObj.cardId}">
+                <figure class="card__images-wrapper">
+                    <object 
+                        class="card__image"
+                        data="${cardObj.img}"
+                        type="image/jpg">
+                        <img class="card__image" src="./../../assets/backupcard.png" />
+                    </object>
+
+                    <img 
+                        class="card__image-back" 
+                        src="/../../assets/back.png"
+                        alt="Default card reverse" >
+                </figure>
+                <h4 class="card__title" >${ cardObj.name ? cardObj.name : cardObj.cardId }</h4>
+                <div class="card__description">
+                    <p class="card__description">${ cardObj.text ? cardObj.text : 'No text' }</p>
+                    <p class="card__flavor">${ cardObj.flavor ? ("<b>Flavor:</b> " + cardObj.flavor) : '<b>Flavor:</b> No info'}</p>
+                    <p class="card__rarity">${ cardObj.rarity ? ("<b>Rarity:</b> " + cardObj.rarity) : '<b>Rarity:</b> No info'}</p>
+                    <p class="card__cardSet">${ cardObj.cardSet ? ("<b>Card Set:</b> " + cardObj.cardSet) : '<b>Card Set:</b> No info'}</p>
+                    <p class="card__type">${ cardObj.type ? ("<b>Type:</b> " + cardObj.type) : '<b>Type:</b>  No info'}</p>
+                    <p class="card__cost">${ cardObj.cost ? ("<b>Cost:</b> " + cardObj.cost) : '<b>Cost:</b>  No info'}</p>
+                    <p class="card__attack">${ cardObj.attack ? ("<b>Attact:</b> " + cardObj.attack) : '<b>Attact:</b> No info'}</p>
+                    <p class="card__health">${ cardObj.health ? ("<b>Health:</b> " + cardObj.health) : '<b>Health:</b> No info'}</p>
+                    <p class="card__race">${ cardObj.race ? ("<b>Race:</b> " + cardObj.race) : '<b>Race:</b> No info'}</p>
+                    <p class="card__durability">${ cardObj.durability ? ("<b>Durability:</b> " + cardObj.durability) : '<b>Durability:</b> No info'}</p>
+                    <p class="card__class">${ cardObj.playerClass ? ("<b>Class:</b> " + cardObj.playerClass) : '<b>Class:</b> No info'}</p>
+                </div>            
+            </article>
+        `
+    }
+
     getCardMarkup(cardObj) {
         return `
             <article class="card" data-cardId="${ cardObj.cardId}">
@@ -30,20 +64,11 @@ class CardView extends View {
                         alt="Default card reverse" >
                 </figure>
                 <h4 class="card__title" >${ cardObj.name ? cardObj.name : cardObj.cardId }</h4>
-                <p class="card__description">${ cardObj.text ? cardObj.text : 'No text' }</p>
-                <p class="card__rarity">${ cardObj.rarity ? ("Rarity: " + cardObj.rarity) : 'Rarity: No info'}</p>
-                <p class="card__cardSet">${ cardObj.cardSet ? ("Card Set: " + cardObj.cardSet) : 'Card Set: No info'}</p>
-                <p class="card__type">${ cardObj.type ? ("Type: " + cardObj.type) : 'Type: No info'}</p>
-                <p class="card__cost">${ cardObj.cost ? ("Cost: " + cardObj.cost) : 'Cost: No info'}</p>
-                <p class="card__attack">${ cardObj.attack ? ("Attact: " + cardObj.attack) : 'Attack: No info'}</p>
-                <p class="card__health">${ cardObj.health ? ("Health: " + cardObj.health) : 'Health: No info'}</p>
-                <p class="card__race">${ cardObj.race ? ("Race: " + cardObj.race) : 'Race: No info'}</p>
-                <p class="card__flavor">${ cardObj.flavor ? ("Flavor: " + cardObj.flavor) : 'Flavor: No info'}</p>
-                <p class="card__durability">${ cardObj.durability ? ("Durability: " + cardObj.durability) : 'Durability: No info'}</p>
-                <p class="card__class">${ cardObj.playerClass ? ("Class: " + cardObj.playerClass) : 'Class: No info'}</p>
             </article>
         `
     }
+
+
 
     init () {
         
